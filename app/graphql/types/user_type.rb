49f2021acffeac9f3c031ab3632f83161acfc2ac
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-12-05 14:44:12 -0700

    Update UserType to include fname, lname.

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -4,7 +4,8 @@ module Types
   class UserType < Types::BaseObject
     field :id, ID, null: false
     field :email, String
-    field :password, String
+    field :fname, String
+    field :lname, String
     field :favorites, [Types::FavoriteType], null: true
     field :created_at, GraphQL::Types::ISO8601DateTime, null: false
     field :updated_at, GraphQL::Types::ISO8601DateTime, null: false
