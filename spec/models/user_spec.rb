49473d0698404a09cb3567bbe13f34bd23577e85
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-12-05 11:35:16 -0700

    update factories to test.

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -10,7 +10,8 @@ RSpec.describe User, type: :model do
       user = create(:user, email: 'user@email.com')
       expect(user.id).to be_an Integer
       expect(user.email).to eq('user@email.com')
-      expect(user.password).to eq('password')
+      expect(user.fname).to be_a String
+      expect(user.lname).to be_a String
     end
   end
 end
