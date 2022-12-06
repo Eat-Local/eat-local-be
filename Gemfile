efe4fb2b3b704ee04d318adcf13a05e766ed67c8
dominicod <dominicodonnell99@gmail.com>
2022-12-06 17:39:15 -0600

    Refactor: run rubocop

--- Gemfile
+++ Gemfile
@@ -35,7 +35,6 @@ gem 'graphql'
 # gem 'sqlite3', '~> 1.3.13'
 gem 'graphiql-rails'
 
-
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   # gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
--- app/poros/business.rb
+++ app/poros/business.rb
@@ -1,21 +1,23 @@
-class Business
- attr_reader :type,
-             :title,
-             :address,
-             :rating,
-             :site,
-             :img
+# frozen_string_literal: true
 
- def initialize(data)
-  @type = data[:categories][0][:title]
-  @title = data[:name]
-  # @description = ???
-  @address = data[:location]
-  @rating = data[:rating]
-  @site = data[:url]
-  @img = data[:image_url]
-  # @hours = data[:is_closed]???
-  # @phone = ???
-  # @wheelchair_accessible = exstension???
- end
+class Business
+  attr_reader :type,
+              :title,
+              :address,
+              :rating,
+              :site,
+              :img
+
+  def initialize(data)
+    @type = data[:categories][0][:title]
+    @title = data[:name]
+    # @description = ???
+    @address = data[:location]
+    @rating = data[:rating]
+    @site = data[:url]
+    @img = data[:image_url]
+    # @hours = data[:is_closed]???
+    # @phone = ???
+    # @wheelchair_accessible = exstension???
+  end
 end
