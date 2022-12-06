595d48d5107cccae041906e0c1d76fca9c1bf4d6
Kaelin Sleevi <kaelin.sleevi@outlook.com>
2022-12-06 16:15:02 -0700

    add Yelp PORO skeleton with JSON contract basis.

---
+++ app/poros/yelp_poro.rb
@@ -0,0 +1,27 @@
+class YelpPoro
+ attr_reader
+
+ def initialize(data)
+
+ end
+end
+
+
+# "data": [
+#    {
+#   "type": "restaurant/grocery/brewery": string,
+#   "id": "1": number,
+#   "attributes":  {
+#     "title": "Name": string,
+#     "type": "Italian/Deli": string,
+#     "description": "A short description...": string,
+#     "address": "address/location of business": string,
+#     "rating": "rating": number,
+#     "site": "business's website url": string,
+#     "img": "img src url": string,
+#     "hours": "hours of operation": string,
+#     "phone": "phone number": string
+#     "wheelchair_accessible": {boolean}
+#    }
+#   }
+#  ]
