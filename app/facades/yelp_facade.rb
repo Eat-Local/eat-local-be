64f1b44c96c479b489c1761b8a8c7ecd21b01e95
dominicod <dominicodonnell99@gmail.com>
2022-12-06 18:03:13 -0600

    Refactor: run rubocop

--- app/facades/yelp_facade.rb
+++ app/facades/yelp_facade.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
 
 class YelpFacade
-
   def self.business_details(location, business)
     business_data = YelpService.get_businesses(location, business)[:businesses]
     business_data.map do |data|
