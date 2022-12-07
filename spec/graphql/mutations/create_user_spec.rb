0b6929643a3d8df5b29ee897a04f6bc106c222b0
Erik Riggs <riggs.erik@gmail.com>
2022-12-06 20:38:58 -0700

    refactor: fix rubocop offenses

--- spec/graphql/mutations/create_user_spec.rb
+++ spec/graphql/mutations/create_user_spec.rb
@@ -1,55 +1,57 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe Mutations::CreateUser, type: :graphql do
   it 'creates a user' do
     query = <<~GQL
-    mutation {
-        createUser(input: {
-         email: "TimmyT@fakeemail.com",
-         fname: "Tim",
-         lname: "Timmyson"
-        }) {
-         user {
-           id,
-           email,
-           fname,
-           lname
-         }
-         errors
-        }
-        }
+      mutation {
+          createUser(input: {
+           email: "TimmyT@fakeemail.com",
+           fname: "Tim",
+           lname: "Timmyson"
+          }) {
+           user {
+             id,
+             email,
+             fname,
+             lname
+           }
+           errors
+          }
+          }
     GQL
 
     result = EatLocalBeSchema.execute(query)
 
     expect(result['data']['createUser']['user']).to have_key('id')
-    expect(result['data']['createUser']['user']['email']).to eq("TimmyT@fakeemail.com")
-    expect(result['data']['createUser']['user']['fname']).to eq("Tim")
-    expect(result['data']['createUser']['user']['lname']).to eq("Timmyson")
+    expect(result['data']['createUser']['user']['email']).to eq('TimmyT@fakeemail.com')
+    expect(result['data']['createUser']['user']['fname']).to eq('Tim')
+    expect(result['data']['createUser']['user']['lname']).to eq('Timmyson')
     expect(result['data']['createUser']['errors']).to eq([])
   end
 
   it 'throws an error when email is already used' do
-    user = create(:user, email: "TimmyT@fakeemail.com")
+    create(:user, email: 'TimmyT@fakeemail.com')
     query = <<~GQL
-    mutation {
-        createUser(input: {
-         email: "TimmyT@fakeemail.com",
-         fname: "Tim",
-         lname: "Timmyson"
-        }) {
-         user {
-           id,
-           email,
-           fname,
-           lname
-         }
-         errors
-        }
-        }
+      mutation {
+          createUser(input: {
+           email: "TimmyT@fakeemail.com",
+           fname: "Tim",
+           lname: "Timmyson"
+          }) {
+           user {
+             id,
+             email,
+             fname,
+             lname
+           }
+           errors
+          }
+          }
     GQL
     result = EatLocalBeSchema.execute(query)
     expect(result['data']['createUser']).to eq(nil)
-    expect(result['errors'][0]['message']).to eq("Cannot return null for non-nullable field CreateUserPayload.user")
+    expect(result['errors'][0]['message']).to eq('Cannot return null for non-nullable field CreateUserPayload.user')
   end
 end
