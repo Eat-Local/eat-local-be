ba7b29aa40feb23cf0212b5a8bc5d875bdf3fcd5
dominicod <dominicodonnell99@gmail.com>
2022-12-07 11:22:23 -0600

    Feat: introduce new object logic

--- app/poros/business.rb
+++ app/poros/business.rb
@@ -1,23 +1,25 @@
 # frozen_string_literal: true
 
 class Business
-  attr_reader :type,
-              :title,
-              :address,
+  attr_reader :title,
+              :venue_type,
+              :display_address,
               :rating,
               :site,
-              :img
+              :img,
+              :is_closed,
+              :display_phone,
+              :coordinates
 
   def initialize(data)
-    @type = data[:categories][0][:title]
     @title = data[:name]
-    # @description = ???
-    @address = data[:location]
+    @venue_type = data[:categories][0][:title]
+    @display_address = data[:location]
     @rating = data[:rating]
     @site = data[:url]
     @img = data[:image_url]
-    # @hours = data[:is_closed]???
-    # @phone = ???
-    # @wheelchair_accessible = exstension???
+    @is_closed = data[:is_closed]
+    @display_phone = data[:display_phone]
+    @coordinates = data[:coordinates]
   end
 end
--- app/serializers/business_serializer.rb
+++ app/serializers/business_serializer.rb
@@ -2,5 +2,13 @@
 
 class BusinessSerializer
   include JSONAPI::Serializer
-  attributes :title, :venue_type, :display_address, :rating, :site, :img, :is_closed, :price, :display_phone, :coordinates
+  attributes :title,
+             :venue_type,
+             :display_address,
+             :rating, :site,
+             :img,
+             :is_closed,
+             :price,
+             :display_phone,
+             :coordinates
 end
