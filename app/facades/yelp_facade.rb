f775394b7b6cd6488d564bb67c1773132520d52e
dominicod <dominicodonnell99@gmail.com>
2022-12-06 18:02:36 -0600

    Test: write test for business facade

--- app/facades/yelp_facade.rb
+++ app/facades/yelp_facade.rb
@@ -1,9 +1,11 @@
+# frozen_string_literal: true
+
 class YelpFacade
 
- def self.business_details(location, business)
-  business_data = YelpService.get_businesses(location, business)
-  businesses = business_data.map do |data|
-   Business.new(data)
+  def self.business_details(location, business)
+    business_data = YelpService.get_businesses(location, business)[:businesses]
+    business_data.map do |data|
+      Business.new(data)
+    end
   end
- end
 end
