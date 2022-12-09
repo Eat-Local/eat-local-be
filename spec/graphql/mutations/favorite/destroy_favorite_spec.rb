ef78b8b7e70bc10f096235604c9f1296bfda04e1
Erik Riggs <riggs.erik@gmail.com>
2022-12-08 19:52:59 -0700

    test: create test for destroy favorite mutation

---
+++ spec/graphql/mutations/favorite/destroy_favorite_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+RSpec.describe Mutations::DestroyFavorite, type: :graphql do
+  it 'deletes a favorites by user' do
+    create(:user, id: 1)
+    create(:favorite, id: 1, user_id: 1)
+    query = <<~GQL
+      mutation {
+          destroyFavorite(id: 1)
+          {
+             id,
+             title,
+             venueType,
+             address,
+             rating,
+             url,
+             image,
+             isClosed,
+             phone,
+             userId
+
+          }
+        }
+    GQL
+
+    result = EatLocalBeSchema.execute(query)
+  end
+end
