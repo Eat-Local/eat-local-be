994f17bce47847dc241d4ad02a14c9ec7d3cb15c
Benjamin Randolphgit config --global user.email neb417@gmail.comgit config --global init.defaultBranch maingit config --global core.editor atom <neb417@gmail.com>
2022-12-13 09:58:49 -0700

    reinstate migration.

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,27 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_12_03_154914) do
+ActiveRecord::Schema.define(version: 2022_12_03_163044) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "favorites", force: :cascade do |t|
+    t.string "title"
+    t.string "venue_type"
+    t.string "address"
+    t.float "rating"
+    t.string "url"
+    t.string "image"
+    t.string "price"
+    t.string "phone"
+    t.float "latitude"
+    t.float "longitude"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_favorites_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email"
@@ -24,4 +41,5 @@ ActiveRecord::Schema.define(version: 2022_12_03_154914) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "favorites", "users"
 end
